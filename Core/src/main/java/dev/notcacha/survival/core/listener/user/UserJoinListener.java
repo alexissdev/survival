c0a32af01d99fe2266a75dab34cd3eeeee259189
cassha <notcacha1998@gmail.com>
2021-03-10 13:41:41 -0300

    Added the tag applier to UserJoinListener

--- Core/src/main/java/dev/notcacha/survival/core/listener/user/UserJoinListener.java
+++ Core/src/main/java/dev/notcacha/survival/core/listener/user/UserJoinListener.java
@@ -3,6 +3,9 @@ package dev.notcacha.survival.core.listener.user;
 import dev.notcacha.survival.api.event.user.UserJoinEvent;
 import dev.notcacha.survival.api.scoreboard.ScoreboardProvider;
 import dev.notcacha.survival.api.scoreboard.properties.ScoreboardProperties;
+import dev.notcacha.survival.api.tag.applier.TagApplier;
+import dev.notcacha.survival.api.user.User;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -13,9 +16,20 @@ public class UserJoinListener implements Listener {
 
     @Inject
     private ScoreboardProvider scoreboardProvider;
+    @Inject
+    private TagApplier tagApplier;
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onUserJoin(UserJoinEvent event) {
-        scoreboardProvider.setup(event.getPlayer(), ScoreboardProperties.Type.DEFAULT);
+        Player player = event.getPlayer();
+
+        scoreboardProvider.setup(player, ScoreboardProperties.Type.DEFAULT);
+
+        User.TagCompound userTagCompound = event.getUser().getTagCompound();
+
+        if (userTagCompound.getTag() !=  null) {
+            tagApplier.apply(player, userTagCompound.getTag());
+        }
+
     }
 }
